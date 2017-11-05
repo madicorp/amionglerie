729e71ab3af5503b0c44a884b778b3d3eb1b8103
Sega Diop <diop.sega@econostic.net>
2017-11-05 10:55:11 +0000

    Update 2017-11-05T10:55:07.492Z

--- scripts/vuejs/firebase.config.js
+++
@@ -1,1 +0,0 @@
-"use strict";var firebaseConfig={apiKey:"AIzaSyCB9Ar_Smc4f-i7dQYvEMb_G3HSAR_TVyE",authDomain:"ami-onglerie.firebaseapp.com",databaseURL:"https://ami-onglerie.firebaseio.com",projectId:"ami-onglerie",storageBucket:"ami-onglerie.appspot.com",messagingSenderId:"1091800957494"},firebaseApp=firebase.initializeApp(firebaseConfig),db=firebaseApp.database();
